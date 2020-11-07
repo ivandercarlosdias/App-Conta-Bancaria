b3e78ca938c6c6ee568cfd1ff15d35aed4f3497d
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-11-07 18:26:35 -0300

    adicionada parcialmente função de login

--- script.js
+++ script.js
@@ -3,38 +3,42 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
-// Data
-const account1 = {
-    owner: "Jonas Schmedtmann",
+// Dados das contas
+const conta1 = {
+    nome: "Jonas Schmedtmann",
     movimentos: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    jurosRate: 1.2, // %
+    taxaJuros: 1.2, // %
+    usuario: "jonas",
     pin: 1111,
 };
 
-const account2 = {
-    owner: "Jessica Davis",
+const conta2 = {
+    nome: "Jessica Davis",
     movimentos: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    jurosRate: 1.5,
+    taxaJuros: 1.5,
+    usuario: "jessica",
     pin: 2222,
 };
 
-const account3 = {
-    owner: "Steven Thomas Williams",
+const conta3 = {
+    nome: "Steven Thomas Williams",
     movimentos: [200, -200, 340, -300, -20, 50, 400, -460],
-    jurosRate: 0.7,
+    taxaJuros: 0.7,
+    usuario: "steven",
     pin: 3333,
 };
 
-const account4 = {
-    owner: "Sarah Smith",
+const conta4 = {
+    nome: "Sarah Smith",
     movimentos: [430, 1000, 700, 50, 90],
-    jurosRate: 1,
+    taxaJuros: 1,
+    usuario: "sarah",
     pin: 4444,
 };
 
-const accounts = [account1, account2, account3, account4];
+const contas = [conta1, conta2, conta3, conta4];
 
-// Elements
+// Elementos DOM
 const labelWelcome = document.querySelector(".welcome");
 const labelDate = document.querySelector(".date");
 const labelSaldo = document.querySelector(".saldo__valor");
@@ -52,7 +56,7 @@ const btnLoan = document.querySelector(".form__btn--loan");
 const btnClose = document.querySelector(".form__btn--close");
 const btnSort = document.querySelector(".btn--sort");
 
-const inputLoginUsername = document.querySelector(".login__input--user");
+const inputLoginUsuario = document.querySelector(".login__input--usuario");
 const inputLoginPin = document.querySelector(".login__input--pin");
 const inputTransferTo = document.querySelector(".form__input--to");
 const inputTransferAmount = document.querySelector(".form__input--amount");
@@ -61,13 +65,27 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
+// Variáveis globais utilizadas
+let usuarioAtivo;
 
-// TODO: Fazer fn que cria username para os usuarios
+// Login
+btnLogin.addEventListener("click", function (e) {
+    e.preventDefault(); // não deixa realizar o submit no form
+    usuarioAtivo = contas.find(conta => conta.usuario === inputLoginUsuario.value);
+    console.log(usuarioAtivo);
+    if (usuarioAtivo?.pin === Number(inputLoginPin.value)) {
+        labelWelcome.textContent = `Seja bem-vindo, ${usuarioAtivo.nome.split(" ")[0]}!`; // exibe mensagem boas vindas
+        containerApp.style.opacity = 100; // exibe app
+        // exibe movimentação
+        // exibe balanço
+        // exibe sumário
+    }
+});
 
 // Mostra a movimentação da conta
 const displayMovimentos = function (movimentos) {
     containerMovimentos.innerHTML = "";
-    movimentos.forEach(function (value, i) {
+    movimentos.forEach((value, i) => {
         const tipo = value > 0 ? "entrada" : "saida";
         const movimentoItemHTML = `
             <div class="movimentos__item">
@@ -79,27 +97,24 @@ const displayMovimentos = function (movimentos) {
         containerMovimentos.insertAdjacentHTML("afterbegin", movimentoItemHTML);
     });
 };
-displayMovimentos(account1.movimentos);
+displayMovimentos(conta1.movimentos);
 
 // Calcula e exibe o saldo da conta
 const calcDisplaySaldo = function (movimentos) {
-    const saldo = movimentos.reduce(function (total, item) {
-        return total + item;
-    }, 0);
+    const saldo = movimentos.reduce((total, item) => total + item, 0);
     labelSaldo.textContent = `R$ ${saldo}`;
 };
-calcDisplaySaldo(account1.movimentos);
+calcDisplaySaldo(conta1.movimentos);
 
-// Calcula e exige as estatiscas da conta (tudo que foi creditado, debitado e os juros ganhos com os depositos)
+// Calcula e exige as estatiscas da conta
 const calcDisplaySumario = function (movimentos) {
-    // Soma os depósitos
     const entrada = movimentos.filter(movimento => movimento > 0).reduce((total, movimento) => total + movimento, 0);
-    labelEntrada.textContent = `R$ ${entrada}`;
-    // Soma os saques
+    labelEntrada.textContent = `R$ ${entrada}`; // exibe soma depósitos
+
     const saida = movimentos.filter(movimento => movimento < 0).reduce((total, movimento) => total + movimento, 0);
-    labelSaida.textContent = `R$ ${Math.abs(saida)}`;
-    // Juros que irá ganhar com os depósitos
+    labelSaida.textContent = `R$ ${Math.abs(saida)}`; // exibe soma saques
+
     const juros = movimentos.filter(entrada => entrada > 0).map(entrada => (entrada * 1.2) / 100).filter(entrada => entrada >= 1).reduce((total, entrada) => total + entrada, 0);
-    labelJuros.textContent = `R$ ${juros}`;
+    labelJuros.textContent = `R$ ${juros}`; // exibe juros que ganhou com os depósitos
 };
-calcDisplaySumario(account1.movimentos);
+calcDisplaySumario(conta1.movimentos);
