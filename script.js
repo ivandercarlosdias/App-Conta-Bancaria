2fb4c14e87a3adb6992b8c0992e740f79cdda2e8
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-11-07 10:06:36 -0300

    adicionada função displayMovimentos

--- script.js
+++ script.js
@@ -6,28 +6,28 @@
 // Data
 const account1 = {
     owner: "Jonas Schmedtmann",
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    movimentos: [200, 450, -400, 3000, -650, -130, 70, 1300],
     interestRate: 1.2, // %
     pin: 1111,
 };
 
 const account2 = {
     owner: "Jessica Davis",
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    movimentos: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     interestRate: 1.5,
     pin: 2222,
 };
 
 const account3 = {
     owner: "Steven Thomas Williams",
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    movimentos: [200, -200, 340, -300, -20, 50, 400, -460],
     interestRate: 0.7,
     pin: 3333,
 };
 
 const account4 = {
     owner: "Sarah Smith",
-    movements: [430, 1000, 700, 50, 90],
+    movimentos: [430, 1000, 700, 50, 90],
     interestRate: 1,
     pin: 4444,
 };
@@ -44,7 +44,7 @@ const labelSumInterest = document.querySelector(".summary__value--interest");
 const labelTimer = document.querySelector(".timer__clock");
 
 const containerApp = document.querySelector(".app");
-const containerMovements = document.querySelector(".movements");
+const containerMovimentos = document.querySelector(".movimentos");
 
 const btnLogin = document.querySelector(".login__btn");
 const btnTransfer = document.querySelector(".form__btn--transfer");
@@ -61,3 +61,19 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 /////////////////////////////////////////////////
+
+const displayMovimentos = function (movimentos) {
+    movimentos.forEach(function (value, i) {
+        const tipo = value > 0 ? "deposito" : "saque";
+        const movimentoItemHTML = `
+            <div class="movimentos__item">
+                <div class="movimentos__tipo movimentos__tipo--${tipo}">${i + 1}: ${tipo}</div>
+                <div class="movimentos__data">3 dias atrás</div>
+                <div class="movimentos__valor">${value}</div>
+            </div>
+        `;
+        containerMovimentos.insertAdjacentHTML("afterbegin", movimentoItemHTML);
+    });
+};
+
+displayMovimentos(account1.movimentos);
