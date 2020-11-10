4dcce900e6b8726bc7cc851afbb054cf5eced8d2
Ivander Carlos Dias <ivandercarlos@gmail.com>
2020-11-09 21:44:02 -0300

    alterado objeto das contas, incluído propriedades

--- script.js
+++ script.js
@@ -5,50 +5,44 @@
 
 // Dados das contas
 const c1 = {
-    nome: "Jonas Schmedtmann",
-    movimentos: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    nome: 'Jonas Schmedtmann',
+    movimentos: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
     rendimento: 1.2, // %
-    usuario: "jonas",
     pin: 1111,
+    movimentosDatas: [
+        '2019-11-18T21:31:17.178Z',
+        '2019-12-23T07:42:02.383Z',
+        '2020-01-28T09:15:04.904Z',
+        '2020-04-01T10:17:24.185Z',
+        '2020-05-08T14:11:59.604Z',
+        '2020-05-27T17:01:17.194Z',
+        '2020-07-11T23:36:17.929Z',
+        '2020-07-12T10:51:36.790Z',
+    ],
+    currency: 'R$',
+    local: 'pt-BR',
 };
 
 const c2 = {
-    nome: "Jessica Davis",
+    nome: 'Jessica Davis',
     movimentos: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
     rendimento: 1.5,
-    usuario: "jessica",
     pin: 2222,
+    movimentosDatas: [
+        '2019-11-01T13:15:33.035Z',
+        '2019-11-30T09:48:16.867Z',
+        '2019-12-25T06:04:23.907Z',
+        '2020-01-25T14:18:46.235Z',
+        '2020-02-05T16:33:06.386Z',
+        '2020-04-10T14:43:26.374Z',
+        '2020-06-25T18:49:59.371Z',
+        '2020-07-26T12:01:20.894Z',
+    ],
+    currency: 'USD',
+    local: 'en-US',
 };
 
-const c3 = {
-    nome: "Steven Thomas Williams",
-    movimentos: [200, -200, 340, -300, -20, 50, 400, -460],
-    rendimento: 0.7,
-    pin: 3333,
-};
-
-const c4 = {
-    nome: "Sarah Smith",
-    movimentos: [430, 1000, 700, 50, 90],
-    rendimento: 1,
-    pin: 4444,
-};
-
-const c5 = {
-    nome: "Ivander Dias",
-    movimentos: [10, 20, 500, -50, 35],
-    rendimento: 1,
-    pin: 5555,
-};
-
-const c6 = {
-    nome: "Giovanna Bueno",
-    movimentos: [1, 1.50, -50, -100, -300],
-    rendimento: 1.2,
-    pin: 6666,
-};
-
-const contas = [c1, c2, c3, c4, c5, c6];
+const contas = [c1, c2];
 
 // Elementos DOM
 const labelWelcome = document.querySelector(".welcome");
@@ -167,7 +161,7 @@ btnTransferir.addEventListener("click", function(e) {
     if (valor > 0 && valor <= contaAtiva.saldo && receberTransferencia && receberTransferencia !== contaAtiva) {
         // debita e credita os valores nas contas
         receberTransferencia.movimentos.push(valor);
-        contaAtiva.movimentos.push(-valor);
+        console.log(contaAtiva.movimentos.push(-valor));
         // atualiza UI
         atualizaUI(contaAtiva);
     }
